a4774d571e881c8da983fd55638e80d2266784e5
Masy Mohamed <202124813@o6u.edu.eg>
2026-05-17 18:57:19 +0300

    fix(prisma): ignore TS constructor signature error

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+// @ts-ignore
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
